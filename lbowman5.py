3b20fb44d0f3897579fcfc7e2f5e2bfea27cc0ff
LB <lauren.bowmancu21@gmail.com>
2026-08-20 01:04:52 +0000

    lab5

---
+++ lbowman5.py
@@ -0,0 +1,28 @@
+#Lauren Bowman
+#CTC389
+#Lab 5
+
+numevens = [22,24,26,28,30,32,34,36,38,40]
+
+print ("No Loop")
+
+print (numevens[0])
+print (numevens[1])
+print (numevens[2])
+print (numevens[3])
+print (numevens[4])
+print (numevens[5])
+print (numevens[6])
+print (numevens[7])
+print (numevens[8])
+print (numevens[9])
+
+print ("With Loop")
+
+for i in numevens:
+    print(i)
+
+words = ["mango", "durian", "lychee", "jackfruit", "ube"]
+for i in words:
+    print (i)
+

02902caf03abd38cb387fc5dbca0c37d72499a5d
LB <lauren.bowmancu21@gmail.com>
2026-09-10 02:58:55 +0000

    midtermquestion1

---
+++ lbowmanmt1.py
@@ -0,0 +1,14 @@
+#Lauren Bowman
+# Midterm Q1
+#CTC 389
+#9-9-2026
+
+name = input("Hi,what's your name? ")
+number = int(input("Please enter a number: "))
+add = int(input("How much do you want to add to that number? "))
+total = number + add
+
+print (name, "you entered", number, "and added", add, "to get a total of", total)
+
+
+

b5c13884a4a52dd28fdddeb120e0a7cc0b524021
LB <lauren.bowmancu21@gmail.com>
2026-09-10 03:10:27 +0000

    midtermquestion2

---
+++ lbowmanmt2.py
@@ -0,0 +1,17 @@
+#Lauren Bowman
+#CTC389
+#Midterm Question 2
+#9-9-2026
+
+def area (b, h):
+    return b * h
+
+base = float(input("What is the base of the rectangle?  "))
+height = float(input("What is the height of the rectangle? "))
+
+arearectangle = area(base, height)
+
+print ("The area of the rectangle is", arearectangle)
+
+
+
